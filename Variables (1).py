aece125bd7531af9a42529ec20de4fe62a0eb652
lalo-lgtm <lalo8074@gmail.com>
2026-01-23 09:58:40 -0600

    mi primer commit

---
+++ Variables (1).py
@@ -0,0 +1,22 @@
+#Variables en Python 
+
+#Python es un lenguaje no tipado
+
+#Detecta el tipo de dato dependiendo del valor
+
+#Comienza con _a-z A-Z 0-9
+
+name = "Luis"
+age  = 19
+height = 1.69
+is_single = True
+
+print(name)
+print(age)
+print(height)
+print(is_single)
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_single))
---
+++ tipos.py
@@ -0,0 +1,15 @@
+# Tipos de datos en python
+# interos - int
+# flotantes - folat
+# strings - str
+# booleanos - bool 
+
+print(15)
+print(3.14)
+print("Hello")
+print(True)
+
+print(type(15))
+print(type(3.14))
+print(type("Hello"))
+print(type(True))
