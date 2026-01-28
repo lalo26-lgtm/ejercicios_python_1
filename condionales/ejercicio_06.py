fb324d8e13676f37ffe753bd0afccef5fc2d17d5
lalo-lgtm <lalo8074@gmail.com>
2026-01-28 16:03:18 -0600

    mi cuarto commit

---
+++ condionales/ejercicio_06.py
@@ -0,0 +1,9 @@
+# programa wue lee una letra y dice si es mayuscula 
+
+letter = input('ingresa una letra: ')
+
+if letter == letter.upper():
+	print('Es una letra Mayuscula!')
+else:
+	print('No es una letra minuscula')
+
