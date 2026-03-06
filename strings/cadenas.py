73d1b74c296a4b5d92f7648a4c3dd8bba31a395f
lalo-lgtm <lalo8074@gmail.com>
2026-03-06 10:17:28 -0600

    mi sexto commit

---
+++ strings/cadenas.py
@@ -0,0 +1,23 @@
+name = "Francisco"
+print(name)
+lastname = "García"
+print(lastname)
+present = "I'm Frack"
+print(present)
+saludo = '"Hola" es "Hello"'
+print(saludo)
+present_2 = 'I\'m Frack'
+print(present_2)
+menu = "Elige una opción: \n1. Opción 1 \n2. Opción 2"
+print(menu)
+
+fullname = 'FracISCo GarcIA'
+print(fullname)
+print(fullname.upper())
+print(fullname.lower())
+print(fullname.capitalize())
+print(fullname.title())
+print(fullname.split(' '))
+
+print('a' in 'aeiou')
+print('hello' .endswith('llo'))
